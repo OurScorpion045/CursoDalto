05b2f3c7a9b97a5f3738a74ff788b2c114671064
OurScorpion045 <sams24484@gmail.com>
2026-02-09 13:22:20 -0700

    Avance: Template Strings y Backtics

---
+++ JavaScript/Template Strings y Backtics/codigo.js
@@ -0,0 +1,20 @@
+html = `
+<style>
+    body {
+        margin: 0;
+    }
+    div {
+        width: 75%;
+        margin: auto;
+        margin-top: 20px;
+        text-align: center;
+        border: 2px solid #060606;
+    }
+</style>
+<div>
+    <h1>Titulo</h1>
+    <h2>Subtitulo</h2>
+</div>
+`
+
+document.write(html);
