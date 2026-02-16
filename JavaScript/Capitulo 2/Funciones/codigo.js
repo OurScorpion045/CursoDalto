21862b898e36c3db53214a362b8a67846e9ab27c
OurScorpion045 <sams24484@gmail.com>
2026-02-16 10:35:02 -0700

    Avance: Funciones

---
+++ JavaScript/Capitulo 2/Funciones/codigo.js
@@ -0,0 +1,16 @@
+// function suma(num1, num2) {
+//     let resultado = num1 + num2;
+//     return resultado;
+// }
+
+// const sumar = function(num1, num2) {
+//     let resultado = num1 + num2;
+//     document.write(resultado);
+// }
+
+const sumar = (num1,num2) => {
+    let resultado = num1 + num2;
+    document.write(resultado);
+}
+
+sumar(3, 4);
