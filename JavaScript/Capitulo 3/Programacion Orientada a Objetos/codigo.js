6aa6a806520516a0728e9e599c9eee7672f4d885
OurScorpion045 <sams24484@gmail.com>
2026-02-17 14:35:08 -0700

    Avance: Programacion Orientada a Objetos

--- JavaScript/Capitulo 3/Programacion Orientada a Objetos/codigo.js
+++ JavaScript/Capitulo 3/Programacion Orientada a Objetos/codigo.js
@@ -1,11 +1,36 @@
-class animal {
+class Animal {
     constructor(especie, edad, color) {
         this.especie = especie;
         this.edad = edad;
         this.color = color;
-        this.informacion = `Soy ${this.especie}, tengo ${this.edad} años y soy de color ${this.color}`;
+        this.informacion = `Soy un ${this.especie}, tengo ${this.edad} años y soy de color ${this.color}`;
+    }
+
+    verInformacion = () => {
+        document.write(this.informacion);
     }
 }
 
-let perro = new animal("Perro", 2, "Negro");
-document.write(perro.informacion);
+class Perro extends Animal {
+    constructor(especie, edad, color, informacion, raza) {
+        super(especie, edad, color, informacion);
+        this.raza = raza;
+    }
+    ladrar() {
+        alert("Guau!!");
+    }
+
+    set setInformacion(newRaza) {
+        this.raza = newRaza
+        this.informacion = `Soy un ${this.especie}, tengo ${this.edad} años, soy de color ${this.color} y mi raza es ${this.raza}`;
+    }
+
+    get getInformacion() {
+        return this.informacion;
+    }
+}
+
+const perro01 = new Perro("Perro", 5, "Negro", "Pitbull Terrier");
+perro01.setInformacion = "Chihuahua";
+//perro01.verInformacion();
+document.write(perro01.getInformacion);
