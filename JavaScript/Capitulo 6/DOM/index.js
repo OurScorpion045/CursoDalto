2460e092dba5678670cf3c907fdd905a7c422fb6
Mario Alejandro Hdez Lopez <sams24484@gmail.com>
2026-05-16 17:20:02 -0700

    Avance: Capitulo 6: DOM

---
+++ JavaScript/Capitulo 6/DOM/index.js
@@ -0,0 +1,7 @@
+const parrafo = document.getElementById("parrafo");
+
+const parrafos = document.getElementsByTagName("p");
+
+const input = document.getElementById("input");
+
+input.setAttribute("type", "text");
